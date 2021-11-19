eb91cb7c85a8fd55eac5f3be51e4d447103ccaf4
Dorin Andrei Dragan <ddragan@bethel.jw.org>
2021-11-19 16:45:54 +0000

    Copied the already configured settings for p10k

--- config.sh
+++ config.sh
@@ -1,8 +1,12 @@
-# install ohmyzsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# echo "Install oh-my-zsh"
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# install powerlevel10k zsh theme
+echo "Install powerlevel10k zsh theme"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# fish like auto-suggestions
+echo "Fish like auto-suggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+cp .p10k.zsh ~/.p10k.zsh
+
+echo "Copy the profile"
+cp .zshrc ~/.zshrc
