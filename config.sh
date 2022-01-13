0f133ed10cd2420b9aadaa4ff3af56fc004bf9f8
dorinandreidragan <dorinandreidragan@users.noreply.github.com>
2022-01-13 21:50:24 +0200

    Uncomment the installation of oh-my-zsh

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
-# echo "Install oh-my-zsh"
-# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Install oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Install powerlevel10k zsh theme"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
